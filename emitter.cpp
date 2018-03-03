8b5862d448e126234972f37dfedb9e7602b87174
abdulilah abdullah <al-shoshan@hotmail.com>
2018-03-03 17:33:24 +0300

    Update emitter.cpp

--- emitter.cpp
+++ emitter.cpp
@@ -1,10 +1,11 @@
 #include "global.h"
+
 void emit(int t, int tval) {
-	string nameOfOp;
+	string nameOfOp="hell";
 	switch (t)
 	{
-	case '+':nameOfOp = "add"; break;
-	case '-':nameOfOp = "sub"; break;
+	case '+':nameOfOp = "add "; break;
+	case '-':nameOfOp = "sub "; break;
 	case '*':nameOfOp = "mult"; break;
 	case '/':nameOfOp = "Rdiv";
 	}
@@ -14,6 +15,7 @@ void emit(int t, int tval) {
 	case '+': case '-': case '*' : case '/':
 		output << "pop r1\n" << "pop r2\n"<< nameOfOp << " r2 ,r1\npush r2\n";
 		//output.put(t);
+		//output.put(' ');
 		break;
 	case DIV:
 		output << "pop r1\npop r2\nDiv r2 ,r1\npush r2\n";
@@ -24,10 +26,28 @@ void emit(int t, int tval) {
 		//output << "MOD ";
 		break;
 	case NUM:
-		output << "push " << tval << endl;
+		output << "push " <<tval<<endl;
+		break;
+	case FID:
+		output << "push " << symtable[tval].lexptr << endl;
 		break;
 	case ID:
 		output << "push " << symtable[tval].lexptr << endl;
+		break;
+	case IF:
+		output << "pop r2\ncomp r2 , 0\nBe else\n";
+		break;
+	case ELSE:
+		output << "else\n";
+		break;
+	case WHILE:
+		output << "while\n";
+		break;
+	case AFTERWHILECOND:
+		output << "pop r2\ncmp r2,0\nendwhile\n";
+		break;
+	case ENDWHILE :
+		output << "while\nendwhile\n";
 		break;
 	default:
 		output << "token " << t << ",tokenval " << tval << endl; 
