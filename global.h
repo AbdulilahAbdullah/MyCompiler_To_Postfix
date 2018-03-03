fcb3edb2de17220351577589cffefade91bc8126
abdulilah abdullah <al-shoshan@hotmail.com>
2018-03-03 17:36:53 +0300

    Update global.h

--- global.h
+++ global.h
@@ -12,10 +12,31 @@ using namespace std;
 #define NUM 256
 #define DIV 257
 #define MOD 258
-#define ID 259
-#define DONE 260
+#define FID 259
+#define ID 260
+#define DONE 261
+#define IF 262
+#define ELSE 263
+#define THEN 264
+#define WHILE 265
+#define DO 266
+#define AFTERWHILECOND 267
+#define ENDWHILE 268
 
 #define SYMMAX 100
+int lookup(char s[]);
+int insert(char s[], int tok);
+int lexan();
+void emit(int t, int tval);
+void expr();
+void term();
+void match(int);
+void factor();
+void error(int m);
+void init();
+void parse();
+void stmt();
+
 extern ofstream output;
 extern ofstream err;
 extern ifstream input;
